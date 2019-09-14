3966373c6652d3158aeeb5a4bed28a6c2cb32d9c
ojSeyi <1509941@rgu.ac.uk>
2019-09-14 17:15:54 +0100

    Swag

---
+++ db_conn.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: DOC
+ */
+
+$servername = "localhost";
+$username = "username";
+$password = "password";
+$dbname = "myDB";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+?>
